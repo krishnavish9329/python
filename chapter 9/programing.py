5ac88adbde538b143cb3e52c40cd61fa0fcb316c
krishnavish9329 <Krishna.vish9329@gmail.com>
2024-10-19 16:19:17 +0800

    doc: add file i/o

--- chapter 9/programing.py
+++ chapter 9/programing.py
@@ -1,23 +1,32 @@
-#f=open("this.txt","r")  #read the file 
-#hi=f.read(20)
-#print(hi)
-#print(type(hi))
-#f.close()
+# f=open("this.txt","r")  #read the file 
+# hi=f.read()
+# print(hi)
+# print(type(hi))
+# f.close()
 
-#f=open("this.txt","w") #write the file but before the write ,all data is cleare 
-#f.write("hello krishna")
-#f.write("hello krishna3")
-#f.close()
+# f=open("this.txt","w") #write the file but before the write ,all data is cleare 
+# f.write("hello krishna\n")
+# f.write("hello krishna3\n")
+# f.write("i am completed my bechelor of engineering at computer scince of engineering form baderia global engineeirng college jabalpur \n")
+# f.write("i have interset in full stack\n ")
+# f.close()
 
-#f=open("this.txt","r")  #read the file using readline
-#hi=f.readline()
-#print(hi)
-#f.close()
+# f=open("this.txt","r")  #read the file using readlinhi=f.readline()
+# hi=f.readline()
+# print(hi)
 
-#with open("helo.txt","a")as f:
+# hi1=f.readline()
+# print(hi1)
+
+# hi2=f.readline()
+# print(hi2)
+
+# f.close()
+
+# with open("this.txt","a")as f:
 #    f.write("\n hello sangam")
 #    f.write("i am genious")
 
-#a,b=2,3
-#a,b,c=4,5,6
-#print(a,b,c)
+a,b=2,3
+a,b,c=4,5,6
+print(a,b,c)
